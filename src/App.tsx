ed5d179ec953ff8a45de2752325100679df073d3
zinzinkyo <zinzinkyo@Mac.lan>
2026-09-11 00:00:59 +0200

    maintain visible focus indicator across sll steps

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import { Routes, Route } from 'react-router-dom'
 import { HomePage } from './pages/HomePage'
 import { StationDetailPage } from './pages/StationDetailPage'
 import { NotFoundPage } from './pages/NotFoundPage'
+import { SkipLink } from './components/SkipLink'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/stations/:id" element={<StationDetailPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+    <>
+      <SkipLink />
+      <div id="contenu-principal" tabIndex={-1}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/stations/:id" element={<StationDetailPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
+    </>
   )
 }
 
